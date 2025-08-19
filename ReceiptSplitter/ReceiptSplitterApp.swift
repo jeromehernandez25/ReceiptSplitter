e65827052b1c72be7439ac8c89dfeec1a0012247
jeromehernandez25 <jeromemh63@gmail.com>
2025-08-19 10:36:59 -0700

    Initial Commit

---
+++ ReceiptSplitter/ReceiptSplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptSplitterApp.swift
+//  ReceiptSplitter
+//
+//  Created by Jerome Hernandez on 8/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ReceiptSplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReceiptSplitterTests/ReceiptSplitterTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptSplitterTests.swift
+//  ReceiptSplitterTests
+//
+//  Created by Jerome Hernandez on 8/19/25.
+//
+
+import Testing
+@testable import ReceiptSplitter
+
+struct ReceiptSplitterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
